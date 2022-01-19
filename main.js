e984d04c30a0b5d4dcc55ee351291db337cb9a71
EC2 Default User <ec2-user@ip-172-31-10-20.ap-northeast-1.compute.internal>
2022-01-19 06:29:01 +0000

    gitの練習-firstcommit

---
+++ main.js
@@ -0,0 +1,17 @@
+function get_calc(btn) {
+      if(btn.value == "=") {
+        document.dentaku.display.value = eval(document.dentaku.display.value);
+      } else if (btn.value == "AC") {
+        document.dentaku.display.value = "";
+      } else {
+        if (btn.value == "×") {
+          btn.value = "*";
+        } else if (btn.value == "÷") {
+          btn.value = "/";
+        } 
+        document.dentaku.display.value += btn.value;
+        document.dentaku.display.multi_btn.value = "×";
+        document.dentaku.display.div_btn.value = "÷";
+      }
+    }
+  
